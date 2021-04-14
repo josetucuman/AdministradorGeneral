4cf76d962a6deda1e14b9614ff50854003c492d6
josetucuman <josegelimergomez@gmail.com>
2021-04-14 20:19:31 -0300

    primer commit

---
+++ src/main/java/com/vyjsoft/AdministradorGeneral/AdministradorGeneralApplication.java
@@ -0,0 +1,13 @@
+package com.vyjsoft.AdministradorGeneral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorGeneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorGeneralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vyjsoft/AdministradorGeneral/AdministradorGeneralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyjsoft.AdministradorGeneral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorGeneralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
